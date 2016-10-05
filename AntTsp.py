904ada20b99f7c54c04c5c9096b0e36ade038953
jake-the-destroyer <jakebwalsh@gmail.com>
2016-10-05 16:44:11 +0100

    Made changes in making the arrays of ants in to one 2d array - all is working fine. Also Tacked on the attribute for pheremone trails as a tuple to the city list

--- AntTsp.py
+++ AntTsp.py
@@ -5,129 +5,9 @@ from random import randint
 ''' 
 First attempt at the ant algorithm to solve some simple TSP problems.  
 Note: The TSP Matrices are fed in to this program as a full Matrix inthe form of a text file
-Weights for the matrices are parsed as doubes and therefore must all be >= 0
 '''
 
 class AntTsp:
-
-  #Method to ake ants visit a town and mark the town as being visited.
-  def visit( ant, town ):
-    print("hello")
-
-  #Method for finding the tour length so far of an ant
-  def tourLength( ant_tour_list, city_list ):
-    #List of lengths of totals of tours.
-    i = len(ant_tour_list[1])
-    length = []
-	
-    #For each ant....
-    for ant in ant_tour_list:
-      ant_total = 0
-      #And for each city in the ant list
-      current_city = ant[0]
-      for city_values in ant[1:]:
-        #Look up the value to travel from previous city to next city
-		
-        next_city = city_values
-		
-        distance = city_list[current_city][next_city]
-        ant_total += distance
-        current_city = next_city
-        #Add on to the total time taken
-		
-      length.append(ant_total)
-	
-    #Return the list of values of total distance for each ant.
-    return length
-    
-  def readFile(libfile):
-
-    '''
-    What I need to do;
-
-    1 - Take in text file of a matrix. (Done)
-    2 - Turn that into a list (Done)
-    3 - Split the list on a space to increase the value of i (Done)
-    4 - Split the list on a new line to increase the value of j (Done)
-    5 - write the values individually to a 2 D array (Done)
-    6 - Return the array as your graph in a matrix of vertices (Done)
-    7 - Thank your grandparents for naming your uncle Bob
-    '''
-
-    j = 0
-    with open(libfile) as f:
-      content = f.readlines()
-      f.close()
-
-    split_list = []
-    i=0
-    for term in content:
-      split_list.append(term.split( ))
-
-    for i in range(len(split_list)):
-      for j in range(len(split_list[i])):
-        split_list[i][j] = int(split_list[i][j])
-    return split_list
-
-  def placeAnts(ant_taboo_list, ant_tour_list):
-    '''
-    Things I have to do 
-    - Place ants on each city 
-    - Update the ant_visited list with the city index of the city that the ant has been places
-    - Update the ant_tour list in index position 0 with the city that the ant has been placed
-    '''
-    city_index = 0
-    ant_index = 0 
-
-    for ant in range ( 0, len(ant_taboo_list)):
-      ant_taboo_list[ant_index][city_index] = 1
-      ant_tour_list[ant_index].append(city_index)
-      city_index = (city_index + 1) % (len(ant_taboo_list[0]))
-      ant_index = ant_index + 1 
-      
-    return ant_taboo_list, ant_tour_list
-
-  def updateTour( tour_list, city_value ):
-    ''' 
-    - Take in a tour list of a given ant
-    - Add the city_index to the list
-    - Return the newly updated list.
-    '''
-    tour_list.append(city_value)
-    return tour_list
-
-  def updateTaboo( visited_list, city_index ):
-    ''' 
-    - Take in a visited list of a given ant
-    - Flip the bit of the index of the city in ant_visited list 
-    - Return the newly updated list
-    '''
-    visited_list[city_index] = 1
-    return visited_list
-
-
-  def move(ant_taboo, ant_tour):
-
-    #For each ant in the list of ants
-    total_ants = len(ant_taboo)
-    for ant_index in range(0, total_ants):
-      #find an array of free cities
-      #free_cities = ant_taboo[ant_index].index(0)
-      free_cities = [i for i, x in enumerate(ant_taboo[ant_index]) if x == 0]  
-      #use a probability to choose the next path NEED TO PROPERLY IMPLEMENT THIS
-      if len(free_cities) > 0:
-        next_city_index = (randint(0,(len(free_cities) - 1)))
-        next_city = free_cities[next_city_index]
-        #update both lists with the values of the new city etc.
-        ant_taboo[ant_index][next_city] = 1
-        ant_tour[ant_index].append(next_city)
-	  
-    return ant_taboo, ant_tour
-	  
-  
-  def pickNext():
-    print("hello")
-
 
   #Initialize all class parameters
 
@@ -144,9 +24,13 @@ class AntTsp:
   #Random number for global trail updating
   globe = 0
 
-  #Random probability of the equation being discarded
-  randomProb = 0
+  #Evaporation Rate.
+  global evaporation_rate
+  evaporation_rate = 0.5
 
+  #Random probability of the equation being discarded
+  global random_prob 
+  random_prob = 1
   #the beow are al subject to tweaking and fixing for optimality
   #Trail intensity variable - open to fiddling and testing
   mew = 0.1
@@ -157,20 +41,162 @@ class AntTsp:
   #Weight of the pheramone of the agorithm
   beta = 0.02
 
+  '''
+  Method for finding the tour length so far of an ant
+  '''
+  def tourLength( ant_tour_list, city_list ):
+    #List of lengths of totals of tours.
+    i = len(ant_tour_list[1])
+    length = []
+	
+    #For each ant....
+    for ant in ant_tour_list:
+      ant_total = 0
+      #And for each city in the ant list
+      current_city = ant[0]
+      for city_values in ant[1:]:
+        #Look up the value to travel from previous city to next city
+		
+        next_city = city_values
+		
+        distance = city_list[current_city][next_city][0]
+        ant_total += distance
+        current_city = next_city
+        #Add on to the total time taken
+		
+      length.append(ant_total)
+	
+    #Return the list of values of total distance for each ant.
+    return length
+
+  '''
+  Method for reading in a file and converting it to
+  a 3d list of cities with distances and tuples
+  '''  
+  def readFile(libfile):
+
+    j = 0
+    with open(libfile) as f:
+      content = f.readlines()
+      f.close()
+
+    split_list = []
+    i=0
+    for term in content:
+      split_list.append(term.split( ))
+
+    for i in range(len(split_list)):
+      for j in range(len(split_list[i])):
+        split_list[i][j] = (int(split_list[i][j]), 0)
+    return split_list
+
+  '''
+  method to initially place ants throughout the graph cyclically 
+  '''
+  def placeAnts(ant_tour):
+
+    city_index = 0
+    ant_index = 0 
+
+    for ant in range ( 0, len(ant_tour)):
+      #ant_taboo_list[ant_index][city_index] = 1
+      ant_tour[ant_index].append(city_index)
+      city_index = (city_index + 1) % (len(full_matrix[0]))
+      ant_index = ant_index + 1 
+      
+    return ant_tour
+
+  '''
+  Method to move an ant to the next available city, based on some 
+  probabilities.
+  '''
+  def move(ant_tour, city_list):
+
+    #For each ant in the list of ants
+    total_ants = len(ant_tour)
+    for ant_index in range(0, total_ants):
+      #find a list of free cities
+      free_cities = [] 
+      for item in range(0, len(city_list)):
+        if item not in ant_tour[ant_index]:
+          free_cities.append(item)
+
+      #free_cities = [i for i, x in enumerate(ant_taboo[ant_index]) if x == 0]  
+      #use a probability to choose the next path NEED TO PROPERLY IMPLEMENT THIS
+      if len(free_cities) > 0:
+        '''
+        Want to do this with an external function....
+        '''
+        #Randomly pick a totally random path...
+        if randint(0, 1) <= random_prob:
+          next_city_index = (randint(0,(len(free_cities) - 1)))
+          next_city = free_cities[next_city_index]
+        #else, use the formula
+        else:
+          print("hello")
+
+        #next_city_index = (randint(0,(len(free_cities) - 1)))
+        #next_city = free_cities[next_city_index]
+        #next_city = self.pickNext(free_cities)
+
+        #update both lists with the values of the new city etc.
+        ant_tour[ant_index].append(next_city)
+	  
+    return ant_tour
+	  
+  '''
+  Method for picking random city using formula or otherwise.
+  '''
+  def pickNext(free_cities):
+    #Randomly pick a totally random path...
+    if randint(0, 1) <= random_prob:
+      next_city_index = (randint(0,(len(free_cities) - 1)))
+      next_city = free_cities[next_city_index]
+    #else, use the formula
+    else:
+      print("hello")
+    return next_city
+
+  '''
+  Method for updating the trails of the ants.
+  '''
+  def updateTrails():
+    for i in range(0, len(full_matrix)):
+      for j in range(0, len(full_matrix)):
+        full_matrix[i][j][1] *= evaporation_rate
+
+    '''
+    for each ant in the list of ants... 
+
+    - find the contribution by dividing Q(normally 500) by the total tour length
+    - update the trail of the whole tour performed by the ants.
+    for (Ant a : ants) {
+       double contribution = Q / a.tourLength();
+       for (int i = 0; i < n - 1; i++) {
+         trails[a.tour[i]][a.tour[i + 1]] += contribution;
+       }
+       trails[a.tour[n - 1]][a.tour[0]] += contribution;
+     }
+    '''
+
+
+
+
+
   #Parsed graph
-  full_matrix = readFile('somerandomstuff.txt')
+  global full_matrix
+  full_matrix  = readFile('somerandomstuff.txt')
 
   #2D List of all the ants vs visited towns, initialized with zeroos
-  ant_visited = [[0 for x in range(len(full_matrix[1]))] for y in range(num_ants )]
+  #ant_visited = [[0 for x in range(len(full_matrix[1]))] for y in range(num_ants )]
 
   #2D list of all tours performed by ants. all of which are, for now empty.
   ant_tour = [[] for y in range(num_ants )]
 
-  ant_visited, ant_tour = placeAnts(ant_visited, ant_tour)
+  ant_tour = placeAnts( ant_tour)
   for cities in range(0, len(full_matrix[0])):
 
-    ant_visited, ant_tour = move(ant_visited, ant_tour)
+    ant_tour = move(ant_tour, full_matrix)
   
-  print(ant_visited)
   print(ant_tour)
   print(tourLength(ant_tour, full_matrix))
