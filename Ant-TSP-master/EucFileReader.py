45af6766751b6c65f249c99b37211f6f7f00489e
jake-the-destroyer <jakebwalsh@gmail.com>
2017-01-19 13:59:37 +0000

    Detecting points to be deleted. Still need to add functionality to detect point of intersection of the detected deletable points

--- Ant-TSP-master/EucFileReader.py
+++ Ant-TSP-master/EucFileReader.py
@@ -9,7 +9,7 @@ def readFile(libfile):
 
   #open the supplied file
   with open(libfile) as f:
-    #content variable assumes list, with each item portaining a line.
+    #content variable assumes list, with each item pandtaining a line.
     content = f.readlines()
     f.close()
 
@@ -46,6 +46,65 @@ def readFile(libfile):
               if real_map[h][j] != 1:
                 real_map[h][j] = 2
                 break
+
+
+  for i in range(len(real_map)):
+    for j in range(len(real_map[i])):
+      if real_map[i][j] == 2:
+
+        left = (i - 1)
+        right = (i + 1)
+        up = (j - 1)
+        down = (j + 1)
+
+        foundLeft = False
+        foundRight = False
+        foundUp = False
+        foundDown = False 
+        keep = False       
+
+        while (foundLeft == False and left >= 0):
+          point = real_map[left][j]
+          if point == 2:
+            foundLeft = True
+          elif point == 1:
+            keep = True
+            foundLeft = True
+          else:
+            left = left - 1
+
+        while (foundRight == False and right < len(real_map)):
+          point = real_map[right][j]
+          if point == 2:
+            foundRight = True
+          elif point == 1:
+            keep = True
+            foundRight = True
+          else:
+            right = right + 1
+
+        while (foundUp == False and up >= 0):
+          point = real_map[i][up]
+          if point == 2:
+            foundUp = True
+          elif point == 1:
+            keep = True
+            foundUp = True
+          else:
+            up = (up - 1)
+
+        while (foundDown == False and down < len(real_map[j])):
+          point = real_map[i][down]
+          if point == 2:
+            foundDown = True
+          elif point == 1:
+            keep = True
+            foundDown = True
+          else:
+            down = (down + 1)
+
+        if (not keep) and (foundLeft or foundRight) and (foundUp or foundDown):
+          print("Delete Point %d %d" % (i, j))
 
 
 '''
